9c3c2a7b1765ba0cbbebf47163d53b6f0647afb2
Abrar Ahmad <siyranabrar12345@gmail.com>
2026-04-29 10:46:43 +0530

    add top navbar to dashboard layout

--- client/src/components/layout/DashboardLayout.jsx
+++ client/src/components/layout/DashboardLayout.jsx
@@ -1,11 +1,13 @@
 import Sidebar from './Sidebar';
+import Navbar from './Navbar';
 import { motion } from 'framer-motion';
 
 export default function DashboardLayout({ children }) {
   return (
     <div className="flex min-h-screen bg-surface-950">
+      <Navbar />
       <Sidebar />
-      <main className="flex-1 lg:ml-64 p-6 md:p-10">
+      <main className="flex-1 lg:ml-64 pt-28 p-6 md:p-10">
         <motion.div
           initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
